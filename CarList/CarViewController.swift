015cea75eb0f80126072a8a904f007aefa88516a
yongjunhan <hanyongjun.dev@gmail.com>
2022-12-02 18:16:09 +0900

    리터럴로 리스트 생성

--- CarList/CarViewController.swift
+++ CarList/CarViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CarViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
     
+    var nameList = ["이상해씨","파이리","꼬부기"]
+    var typeList = ["풀","불","물"]
     
 
     override func viewDidLoad() {
@@ -17,11 +19,25 @@ class CarViewController: UIViewController,UITableViewDataSource, UITableViewDele
     // UITableViewDataSource
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return nameList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? listCell else {
+            return UITableViewCell()
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).png")
+        let newWidth = 150
+        let newHeight = 150
+        let newBg = CGRect(x: 0, y: 0, width: newWidth, height: newHeight)
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        img?.draw(in: newBg)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()?.withRenderingMode(.alwaysOriginal)
+        UIGraphicsEndImageContext()
+        
+        cell.imgView.image = newImage
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.typeLabel.text = typeList[indexPath.row]
         return cell
     }
     
@@ -30,4 +46,10 @@ class CarViewController: UIViewController,UITableViewDataSource, UITableViewDele
         print(indexPath.row)
     }
 
+}
+
+class listCell : UITableViewCell {
+    @IBOutlet weak var imgView : UIImageView!
+    @IBOutlet weak var nameLabel : UILabel!
+    @IBOutlet weak var typeLabel : UILabel!
 }
